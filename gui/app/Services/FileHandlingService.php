5991a1cfd140e1f748fac5da40fb0afa88b0e74b
Adam <kazai.adam@gmail.com>
2025-05-22 22:01:42 +0200

    refactor file handling service

--- gui/app/Services/FileHandlingService.php
+++ gui/app/Services/FileHandlingService.php
@@ -11,10 +11,23 @@ use App\DTOs\DevFileData;
 
 class FileHandlingService
 {
-    private string $devFileListDirectory = '.dev';
+    /**
+     * The directory where the dev files should be stored.
+     *
+     * @var string
+     */
+    private string $devFileListDirectory;
 
-    public function __construct() {}
+    public function __construct() {
+        $this->devFileListDirectory = config('liquid.paths.dev_directory');
+    }
 
+    /**
+     * Get the compiled file content.
+     *
+     * @param string $fileName
+     * @return string
+     */
     public function getCompiledFileContent(string $fileName)
     {
         $fileList = $this->getDevFileList();
@@ -33,6 +46,11 @@ class FileHandlingService
         return $template->render();
     }
 
+    /**
+     * Get the list of dev files.
+     *
+     * @return Collection
+     */
     public function getDevFileList(): Collection
     {
         $devFileListPath = Cache::get(CacheKeys::FILE_SOURCE_DIRECTORY) . '/' . $this->devFileListDirectory;
@@ -41,6 +59,35 @@ class FileHandlingService
             return Cache::get(CacheKeys::DEV_FILE_LIST);
         }
 
+        $devFileCollection = $this->parseDirectory($devFileListPath);
+
+        Cache::put(CacheKeys::DEV_FILE_LIST, $devFileCollection, 60 * 60 * 24);
+
+        return $devFileCollection;
+    }
+
+    /**
+     * Get the file path from the file list.
+     *
+     * @param Collection $fileList
+     * @param string $fileName
+     * @return string
+     */
+    public function getFilePath(Collection $fileList, string $fileName): string
+    {
+        $file = $fileList->whereStrict('name', $fileName);
+
+        return $file->isNotEmpty() ? $file->first()->path : '';
+    }
+
+    /**
+     * Parse the directory and return a collection of DevFileData.
+     *
+     * @param string $directory
+     * @return Collection
+     */
+    public function parseDirectory(string $directory): Collection
+    {
         $iterator = new \RecursiveIteratorIterator(
             new \RecursiveDirectoryIterator($devFileListPath, \FilesystemIterator::SKIP_DOTS)
         );
@@ -58,15 +105,6 @@ class FileHandlingService
             }
         }
 
-        Cache::put(CacheKeys::DEV_FILE_LIST, $devFileCollection, 60 * 60 * 24);
-
         return $devFileCollection;
-    }
-
-    public function getFilePath(Collection $fileList, string $fileName): string
-    {
-        $file = $fileList->whereStrict('name', $fileName);
-
-        return $file->isNotEmpty() ? $file->first()->path : '';
     }
 }
---
+++ gui/config/liquid.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'paths' => [
+        'dev_directory' => env('LIQUID_DEV_DIRECTORY', '.dev'),
+        'content_block_directory' => env('LIQUID_CONTENT_BLOCK_DIRECTORY', 'content_blocks'),
+    ],
+];
